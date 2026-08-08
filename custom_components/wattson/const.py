8e4628dac2106a1b5108cb0eb74d5fc96c82279e
Tokebjerg <150387592+Tokebjerg@users.noreply.github.com>
2026-08-08 09:51:14 +0200

    Fix value-aware reserve planning

--- custom_components/wattson/const.py
+++ custom_components/wattson/const.py
@@ -7,7 +7,7 @@ from homeassistant.const import Platform
 
 DOMAIN = "wattson"
 NAME = "Wattson"
-INTEGRATION_VERSION = "0.26.4"
+INTEGRATION_VERSION = "0.26.5"
 
 PLATFORMS = [
     Platform.SENSOR,
